e9ec0057b4f2af6b8494873c5487b2112d480a8c
WeiHuang <b0979593011@gmail.com>
2023-03-28 11:52:46 +0800

    Initial Commit

---
+++ APP02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APP02
+//
+//  Created by Wei Huang on 2023/3/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
